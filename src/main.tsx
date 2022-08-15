79ece9ee3c8c3ed7ea35ed498df54ecfe1c9adcf
Neto <ss.neto88@gmail.com>
2022-08-15 18:14:02 -0300

    Tailwind CSS and ResetCSS implemented

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "@/styles/reset.css";
+import "@/styles/tailwind.css";
 import { Provider as ProviderJotai } from "jotai";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
