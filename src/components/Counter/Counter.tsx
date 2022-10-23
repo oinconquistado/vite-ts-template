9eecd525e3603ed80930b647887a2b6c10ee8234
Neto <ssneto88@gmail.com>
2022-10-23 20:36:29 -0300

    feat: Counter Component

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,31 @@
+import DataContext from "@/context/DataContext";
+
+const Counter = () => {
+  const { counter } = DataContext();
+  const { count, setCount } = counter;
+
+  return (
+    <div className='mt-8'>
+      <div className='grid place-items-center m-4'>
+        <p className='text-lg '>{count}</p>
+      </div>
+      <div>
+        <button
+          className='bg-slate-200 h-10 w-8'
+          onClick={() => setCount(count + 1)}>
+          +
+        </button>
+        <button
+          className='b-g-slate-200 h-10 w-8'
+          onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        <button className='bg-slate-200 h-10 w-20' onClick={() => setCount(0)}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
