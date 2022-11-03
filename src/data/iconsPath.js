3a778e28f96f78df3cf3ddcd094b00d33b253a3d
Neto <ssneto88@gmail.com>
2022-11-03 01:50:55 -0300

    feat: icons added 😎

---
+++ src/data/iconsPath.js
@@ -0,0 +1,54 @@
+const iconsPath = [
+  {
+    name: "React",
+    path: "./assets/icons/react.svg",
+  },
+  {
+    name: "Vite",
+    path: "./assets/icons/vite.svg",
+  },
+  {
+    name: "TypeScript",
+    path: "./assets/icons/typescript.svg",
+  },
+  {
+    name: "Tailwind",
+    path: "./assets/icons/tailwind.svg",
+  },
+  {
+    name: "Hero Icons",
+    path: "./assets/icons/heroicons.svg",
+  },
+  {
+    name: "React Router",
+    path: "./assets/icons/reactrouter.svg",
+  },
+  {
+    name: "Jotai",
+    path: "./assets/icons/jotai.svg",
+  },
+  {
+    name: "Axios",
+    path: "./assets/icons/axios.svg",
+  },
+  {
+    name: "React Query",
+    path: "./assets/icons/reactquery.svg",
+  },
+
+  {
+    name: "Prettier",
+    path: "./assets/icons/prettier.svg",
+  },
+  {
+    name: "Eslint",
+    path: "./assets/icons/eslint.svg",
+  },
+
+  {
+    name: "Post CSS",
+    path: "./assets/icons/postcss.svg",
+  },
+];
+
+export default iconsPath;
